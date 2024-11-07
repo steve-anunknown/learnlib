461ea243f048a17747300db7c3f0a20e39a0037c
steve-anunknown <steve.anunknown@gmail.com>
2024-11-07 21:15:15 +0200

    subproject for dot reader class

---
+++ learnlib-dot2graph/learnlib-dot2graph/src/main/java/com/example/learnlib/dot2graph/App.java
@@ -0,0 +1,13 @@
+package com.example.learnlib.dot2graph;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ learnlib-dot2graph/src/main/java/com/example/learnlib/dot2graph/DotFileReader.java
@@ -0,0 +1,12 @@
+package com.example.learnlib.dot2graph;
+
+import java.io.File;
+import java.io.IOException;
+
+public class DotFileReader {
+    public void readDotFile(File file) {
+        // Implement DOT file reading logic here
+        Scanner scanner = new Scanner(file);
+    }
+}
+
